6d54dc38a3104cbe2bf8e2d1d3ad186a5112d96f
mGalarnyk <mgalarny@gmail.com>
2016-03-15 22:40:50 -0700

    testing

---
+++ javascripts/application.js
@@ -0,0 +1,9 @@
+var meny = Meny.create({
+  menuElement: document.querySelector( '.meny' ),
+  contentsElement: document.querySelector( '.contents' ),
+  position: 'left',
+  width: 260,
+  threshold: 40,
+  mouse: true,
+  touch: true
+});
